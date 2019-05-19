4e858d7d45c79895ad01494f14e199a6a70aed2d
Brent Lagerman <brent@condensed.io>
2019-05-19 02:10:41 -0400

    fixing the box order of player 2

--- htdocs/js/functions.js
+++ htdocs/js/functions.js
@@ -11,21 +11,23 @@ jQuery(document).ready(function($) {
 
     // don't forget to put a class of hide on the child element directly after the 'make-link' element
 	$(".toggle")
-		.addClass('make-link') // make headings look like links
-		.addClass('header-hidden')
 		.click(function(){
 			var $this = $(this);
-			if( $this.is('.header-shown') ) {
-				$this.next().slideToggle('normal');
-				$this.removeClass('header-shown');
-				$this.addClass('header-hidden');
+			if( $this.is('.box-on') ) {
+				$this.removeClass('box-on');
+				$this.addClass('box-off');
 			} else {
-				$this.next().slideToggle('normal');
-				$this.removeClass('header-hidden');
-				$this.addClass('header-shown');
+				$this.removeClass('box-off');
+				$this.addClass('box-on');
 			}
     		return false;
     })
+    
+    //
+    $('.btn').click(function(){
+        $('.btn').removeClass('on').addClass('off');
+        $(this).removeClass('off').addClass('on');
+    });
     
 	
 });//<--- this is the end of the document ready function don't delete it
